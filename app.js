7eb4a6a94033e85e02067d08a1e20af08dc49d5c
adrienguan2 <adriensh74@gmail.com>
2025-05-12 19:02:58 -0400

    adding an excercise where I make a button that changes some mundane text in html through javascript. This excercise helps understanding dom selection and how to manipulate elements of html with JS.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 document.getElementById("clickMe").addEventListener("click", function() {
     alert("Hello! You clicked the button.");
 });
+
+const magicTarget = document.getElementById("basicText");
+const magicButton = document.getElementById("magicButton");
+
+magicButton.addEventListener("click", function(){
+    magicTarget.textContent = "SYKE HIT BURGER KING AND PLAY NCAA 14";
+});
