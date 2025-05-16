62a8442face6ff430bc5150eb546fe40b2387e6e
adrienguan2 <adriensh74@gmail.com>
2025-05-15 23:00:47 -0400

    cleaned up old button code and added a mouse enter and exiteaster egg. moving on to more mouse events for now

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-// document.getElementById("clickMe").addEventListener("click", function() {
-//     alert("Hello! You clicked the button.");
-// });
-
+/*this section is for experimenting with mouse events, they will be "silly" 
+features for now but eventually they will become more cohesive as web-one is 
+further developed
+*/
 const magicTarget = document.getElementById("basicText");
 const magicButton = document.getElementById("magicButton");
 
 magicButton.addEventListener("click", function(){
     magicTarget.textContent = "SYKE HIT BURGER KING AND PLAY NCAA 14";
+});
+
+const pageSummary = document.getElementById("pageSummary");
+
+
+pageSummary.addEventListener("mouseenter", function(){
+    pageSummary.textContent = "SHOOP DA WOOP";
+});
+
+pageSummary.addEventListener("mouseleave", function(){
+    pageSummary.textContent = "Adrien's web development playground.";
 });
