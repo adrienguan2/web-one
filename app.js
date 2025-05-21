9a218262345962f3efdcafcfff152f5a3fa5406f
adrienguan2 <adriensh74@gmail.com>
2025-05-21 19:47:32 -0400

    added some quick changes to the page description and code comments in the JS file

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+<<<<<<< Updated upstream
 // document.getElementById("clickMe").addEventListener("click", function() {
 //     alert("Hello! You clicked the button.");
 // });
 
+=======
+/*this section is for experimenting with mouse events, they will be simplistic
+features for now but eventually they will become more cohesive as web-one is 
+further developed
+*/
+>>>>>>> Stashed changes
 const magicTarget = document.getElementById("basicText");
 const magicButton = document.getElementById("magicButton");
 
